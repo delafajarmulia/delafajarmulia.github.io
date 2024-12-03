2688cb14a42f6608cd1ba1e49eb9001ecf9df5b4
delafjr <dela.fjr08@gmail.com>
2024-12-03 16:26:39 +0700

    change main.jsx from <StrictMode> to <React.StrictMode>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './routes/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  //   <Router />
+  // </StrictMode>
+  <React.StrictMode>
     <Router />
-  </StrictMode>,
+  </React.StrictMode>
 )
